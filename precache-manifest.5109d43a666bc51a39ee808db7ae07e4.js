8d6ac472da2085790a7c02500518ffd8b9dc2c1e
Aakarsh Arya <aakiarya@yahoo.com>
2020-08-27 13:18:52 -0700

    Updates

---
+++ precache-manifest.5109d43a666bc51a39ee808db7ae07e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbec92a6f364f65e5641af0cc6f45aa",
+    "url": "/RockPaperScissorsAR/index.html"
+  },
+  {
+    "revision": "a34ae4f7d68cd609207e",
+    "url": "/RockPaperScissorsAR/static/css/main.3c714057.chunk.css"
+  },
+  {
+    "revision": "93eac2d751f9b4b39c60",
+    "url": "/RockPaperScissorsAR/static/js/2.003cb978.chunk.js"
+  },
+  {
+    "revision": "b4e9bfab70e3172dde3f42e6c2f1f700",
+    "url": "/RockPaperScissorsAR/static/js/2.003cb978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34ae4f7d68cd609207e",
+    "url": "/RockPaperScissorsAR/static/js/main.34e77bb9.chunk.js"
+  },
+  {
+    "revision": "c88c212f2aa793c9681d",
+    "url": "/RockPaperScissorsAR/static/js/runtime-main.d7a238e4.js"
+  }
+]);
